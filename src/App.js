acf639051d69b8198fe7a3f5f15b488775a9d95e
Yogesh889 <8058yogesh@gmail.com>
2023-10-09 16:36:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './utils/themes';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/HeroSection/Hero';
+import Skills from './components/Skills/Skills';
+import Education from './components/Education/education';
+import Experience from './components/Experience/experience';
+import Projects from "./components/Projects/Projects";
+import ProjectDetails from "./components/ProjectDetails/ProjectDetails";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/footer";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+const Wrapper = styled.div`
+background: ${({ theme }) => theme.gradient};
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+` ;
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal);
+  // Function to toggle between dark and light themes
+  const toggleTheme = () => {
+    setDarkMode(prevDarkMode => !prevDarkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Navbar toggleTheme={toggleTheme} />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+            <Contact />
+          </Wrapper>
+          <Footer />
+          {openModal.state 
+          && <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />}
+        </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
---
+++ src/utils/themes.js
@@ -0,0 +1,39 @@
+// export const darkTheme = {
+//     bg: "#1C1C27",
+//     bgLight: "#1C1E27",
+//     primary: "#854CE6",
+//     text_primary: "#F2F3F4",
+//     text_secondary: "#b1b2b3",
+//     card: "#171721",
+//     card_light: "#191924",
+//     button: "#854CE6",
+//     white: "#FFFFFF",
+//     black: "#000000",
+//   };
+
+export const darkTheme = {
+    bg: "#121212",         // Dark background
+    bgLight: "#1E1E1E",     // Lighter background shade
+    primary: "#4CAF50",     // Vibrant green accent color
+    text_primary: "#FFFFFF", // White primary text color
+    text_secondary:  "#D0D0D0", // Light grey secondary text color
+    card: "#1C1C1C",        // Dark card background
+    card_light: "#292929",   // Lighter card background shade
+    button: "#4CAF50",      // Green button color
+    white: "#FFFFFF",       // White color
+    black: "#000000",       // Black color
+    gradient:  "linear-gradient(38.73deg, rgba(0, 255, 187, 0.15) 0%, rgba(0, 255, 0, 0) 50%), linear-gradient(141.27deg, rgba(0, 128, 0, 0) 50%, rgba(0, 128, 0, 0.15) 100%)",
+    //resume button gradient
+    resume :  "background: hsla(122, 66%, 35%, 1); background: linear-gradient(45deg, hsla(122, 66%, 35%, 1) 0%, hsla(92, 66%, 35%, 1) 100%); background: -moz-linear-gradient(45deg, hsla(122, 66%, 35%, 1) 0%, hsla(92, 66%, 35%, 1) 100%); background: -webkit-linear-gradient(45deg, hsla(122, 66%, 35%, 1) 0%, hsla(92, 66%, 35%, 1) 100%);"
+  
+  };
+  
+  export const lightTheme = {
+    bg: "#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary: "#be1adb",
+    text_primary: "#111111",
+    text_secondary: "#48494a",
+    card: "#FFFFFF",
+    button: "#5c5b5b",
+  };
